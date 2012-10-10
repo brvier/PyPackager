644e5cc04cdfb61a07d9cb5b93cfed8c2577f8d1
Benoit HERVIER <khertan@khertan.net>
2012-10-10 06:53:59 +0200

    Experimental spec file generation for COBS (add missing file in the git repository)

--- make.py
+++ make.py
@@ -28,23 +28,23 @@ if __name__ == "__main__":
     except:
         pass
 
-    p=pypackager.PyPackager("pypackager")
+    p = pypackager.PyPackager("pypackager")
     p.display_name = 'PyPackager'
     p.version = pypackager.__version__
     p.buildversion = __build__
-    p.description="Generate simple deb or source deb from python"
-    p.upgrade_description="Add support for harmattan"
-    p.author=__author__
-    p.maintainer=__author__
-    p.email=__mail__
+    p.description = "Generate simple deb or source deb from python"
+    p.upgrade_description = "Add support for harmattan"
+    p.author = __author__
+    p.maintainer = __author__
+    p.email = __mail__
     p.depends = "python"
     p.suggests = "khteditor"
-    p.section="user/development"
-    p.arch="armel"
-    p.urgency="low"
-    p.icon='pypackager.png'
-    p.distribution="harmattan"
-    p.repository="Khertan Repository"
+    p.section = "user/development"
+    p.arch = "armel"
+    p.urgency = "low"
+    p.icon = 'pypackager.png'
+    p.distribution = "harmattan"
+    p.repository = "Khertan Repository"
     p.bugtracker = 'http://github.com/khertan/PyPackager/issues'
     p.changelog = pypackager.__changelog__
     p.maemo_flags = 'visible'
@@ -54,22 +54,29 @@ if __name__ == "__main__":
     files = []
 
     #Remove pyc and pyo
-    for filepath in glob(os.path.join(os.path.dirname(__file__), p.name, '*.pyc')):
+    for filepath in glob(os.path.join(os.path.dirname(__file__),
+                                      p.name, '*.pyc')):
         os.remove(filepath)
     #Remove pyc and pyo
-    for filepath in glob(os.path.join(os.path.dirname(__file__), p.name, '*.pyo')):
+    for filepath in glob(os.path.join(os.path.dirname(__file__),
+                                      p.name, '*.pyo')):
         os.remove(filepath)
 
     #Src
-    for root, dirs, fs in os.walk(os.path.join(os.path.dirname(__file__), p.name)):
-      for f in fs:
-        files.append(os.path.join(root, f))
+    for root, dirs, fs in os.walk(
+            os.path.join(os.path.dirname(__file__), p.name)):
+        for f in fs:
+            files.append(os.path.join(root, f))
 
     p["/usr/lib/pymodules/python2.6"] = files
 
-    print p.generate(build_binary=True,build_src=True)
+    print p.generate(build_binary=True, build_src=True)
 
     if not os.path.exists('dists'):
         os.mkdir('dists')
-    for filepath in glob(p.name+'_'+p.version+'-'+p.buildversion+'*'):
-        os.rename(filepath, os.path.join(os.path.dirname(filepath), 'dists', os.path.basename(filepath)))
+
+    for filepath in glob(p.name + '_' + p.version
+                         + '-' + p.buildversion + '*'):
+        os.rename(filepath, os.path.join(os.path.dirname(filepath),
+                                         'dists',
+                                         os.path.basename(filepath)))
